7e50688261d9f432f8220c9cbd10077f8b8a5e98
Oliver Hausdoerfer <ge58guq2@login-03.ai.lrz.de>
2025-03-24 13:47:05 +0100

    update run script

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-python video_demo.py shared_camera=True make_reproj_video=True visual_tracks=True SCENE_DIR=examples/dog_cop3d_565_81664_160332_custom
+# Request interactive shell resource on LRZ AI Cluster:
+# srun --pty --container-mounts=./vggsfm/:/workspaces/vggsfm --container-image="./olivertum+vggsfm+latest.sqsh" bash
+
+python video_demo.py shared_camera=True make_reproj_video=True visual_tracks=True 'SCENE_DIR="examples/Thursday, March 20, 2025 5:55:21.359 PM - Walking, Running, Outside"'
